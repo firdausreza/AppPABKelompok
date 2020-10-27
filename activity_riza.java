8dca1e15f78080ffbe0116975f78ee17e3b0c52c
rizafarikhul <67588564+rizafarikhul@users.noreply.github.com>
2020-10-27 18:16:15 +0700

    ProfileRiza

---
+++ activity_riza.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_riza extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riza);
+    }
+}
